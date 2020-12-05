6b11fed5952e8e6d4d85701d83529f2f06d5bc4a
karinabondarenko <karinabon998@gmail.com>
2020-12-05 18:42:05 +0200

    counter component

--- ditek1/src/app/counters/counters.component.ts
+++ ditek1/src/app/counters/counters.component.ts
@@ -13,17 +13,39 @@ export class CountersComponent implements OnInit {
   @Input() show2: boolean;
   @Input() show3: boolean;
 
+  public ee1 = 0;
+  public ee2 = 0;
+  public ee3 = 0;
+  public co1 = 0;
+  public co2 = 0;
+  public co3 = 0;
+  public gen11 = 0;
+  public gen12 = 0;
+  public gen13 = 0;
+  public gen21 = 0;
+  public gen22 = 0;
+  public gen23 = 0;
+
   ngOnInit() {
   }
 
   // tslint:disable-next-line:variable-name
   showInfo(number) {
     switch (number) {
-      case 1 : this.show1 = true;
+      case 1 : this.ee1 = Math.floor(Math.random() * 100);
+               this.co1 = Math.floor(Math.random() * 100);
+               this.gen11 = Math.floor(Math.random() * 100);
+               this.gen21 = Math.floor(Math.random() * 100);
                break;
-      case 2 : this.show2 = true;
+      case 2 : this.ee2 = Math.floor(Math.random() * 100);
+               this.co2 = Math.floor(Math.random() * 100);
+               this.gen12 = Math.floor(Math.random() * 100);
+               this.gen22 = Math.floor(Math.random() * 100);
                break;
-      case 3 : this.show3 = true;
+      case 3 : this.ee3 = Math.floor(Math.random() * 100);
+               this.co3 = Math.floor(Math.random() * 100);
+               this.gen13 = Math.floor(Math.random() * 100);
+               this.gen23 = Math.floor(Math.random() * 100);
                break;
     }
   }
