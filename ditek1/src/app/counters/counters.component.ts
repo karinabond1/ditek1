7d35f013d98d793033d40698576f1649aeb182e6
karinabondarenko <karinabon998@gmail.com>
2020-12-06 00:28:21 +0200

    counter component edit

--- ditek1/src/app/counters/counters.component.ts
+++ ditek1/src/app/counters/counters.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {TxData} from 'ethereumjs-tx';
 
 @Component({
   selector: 'app-counters',
@@ -21,7 +22,9 @@ export class CountersComponent implements OnInit {
 
   @Input() contract: any;
 
-  public balances: any;
+  @Input() client: any;
+
+  @Input() contractAddress: any;
 
   public dis = [
     {
@@ -75,9 +78,41 @@ export class CountersComponent implements OnInit {
           val.ee = Math.floor(Math.random() * 5) + 1 + parseFloat(data[0]);
           val.co = Math.floor(Math.random() * 5) + 1 + parseFloat(data[1]);
           this.dis[number].dis = true;
-          this.contract.methods.edit(val.ee, val.co).call().then((dataEdit) => {
+          /*this.contract.methods.edit(val.ee, val.co).send({from: '0x8C7Fd7c3c0f6405FB474Af45588D5b99a7206Af2'}).then((dataEdit) => {
             console.log('change', dataEdit);
-          });
+          });*/
+          // tslint:disable-next-line:prefer-const
+          // TODO
+          let privateKey = new Buffer('e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109', 'hex');
+          const Tx = require('ethereumjs-tx').Transaction;
+          const rawTx = {
+            nonce: '0x00',
+            gasPrice: '0x09184e72a000',
+            gasLimit: '0x2710',
+            to: '0x8C7Fd7c3c0f6405FB474Af45588D5b99a7206Af2',
+            value: '0x00',
+            data: this.contract.methods.edit(val.ee, val.co).encodeABI(),
+          };
+
+          // tslint:disable-next-line:prefer-const
+          let tx = new Tx(rawTx);
+          tx.sign(privateKey);
+          const serializedTx = tx.serialize();
+          this.client.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+            .on('receipt', console.log);
+
+          // const privateKey = new Buffer('6d...', 'hex');
+
+          /*const rawTransaction = {
+            from: '0x8C7Fd7c3c0f6405FB474Af45588D5b99a7206Af2',
+            gasPrice: this.client.utils.toHex(2 * 1e9),
+            gasLimit: this.client.utils.toHex(210000),
+            to: this.contractAddress,
+            value: '0x0',
+            data: this.contract.methods.edit(val.ee, val.co).encodeABI(),
+            nonce: this.client.utils.toHex(2 * 1e9)};*/
+          /*const transaction = new TxData(rawTransaction);
+          transaction.sign(privateKey);*/
         });
       }
     });
