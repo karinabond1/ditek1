261ce873441b87cb0c990bec534c688b61696757
karinabondarenko <karinabon998@gmail.com>
2020-12-05 17:14:14 +0200

    start

--- ditek1/src/app/app.module.ts
+++ ditek1/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import your library
+import { Web3ServiceModule } from 'angular-web3-service';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // Specify your library as an import
+    Web3ServiceModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
