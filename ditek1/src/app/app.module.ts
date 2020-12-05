b38255bac7fef689734686eff0bddbb58e6f1321
karinabondarenko <karinabon998@gmail.com>
2020-12-05 18:12:11 +0200

    counter component

--- ditek1/src/app/app.module.ts
+++ ditek1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CountersComponent } from './counters/counters.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    CountersComponent
   ],
   imports: [
     BrowserModule,
