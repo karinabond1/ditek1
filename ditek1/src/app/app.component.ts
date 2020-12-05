25d957fadbe8f8990a961e268e32559c5e3ce63d
karinabondarenko <karinabon998@gmail.com>
2020-12-05 18:31:32 +0200

    counter component

--- ditek1/src/app/app.component.ts
+++ ditek1/src/app/app.component.ts
@@ -8,6 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ditek1';
 
+  // tslint:disable-next-line:variable-name
+  show_1 = false;
+  // tslint:disable-next-line:variable-name
+  show_2 = false;
+  // tslint:disable-next-line:variable-name
+  show_3 = false;
+
   customers = [
     {image: '', name: 'Арматурный завод', address: '0xDb32930e429391c1DC31C570CaAc56f08275ef23'},
     {image: '', name: 'Бетонный завод', address: '0xdb32930e429391c1dc31c570caac56f08275ef23'},
