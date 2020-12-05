a907afe5defe5c1e5542daf72d0eed2d2c6e5d85
Tempret <21262736+Tempret@users.noreply.github.com>
2020-12-05 18:12:34 +0200

    main markup

--- ditek1/src/app/dashboard/dashboard.component.ts
+++ ditek1/src/app/dashboard/dashboard.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  customers = [
+    {name: 'Арматурный завод', address: '0xDb32930e429391c1DC31C570CaAc56f08275ef23'},
+    {name: 'Бетонный завод', address: '0xdb32930e429391c1dc31c570caac56f08275ef23'},
+  ];
 
   constructor() { }
 
