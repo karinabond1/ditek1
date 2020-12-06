9b0ac7805e2c6d2faf9568df7dc59e8a99ea9502
Tempret <21262736+Tempret@users.noreply.github.com>
2020-12-06 13:24:30 +0200

    recompose conuters

--- ditek1/src/app/counters/counters.component.ts
+++ ditek1/src/app/counters/counters.component.ts
@@ -71,13 +71,16 @@ export class CountersComponent implements OnInit {
    async countInfo(number) {
     this.counters_info.forEach((val: any, key: any) => {
       if ( key === number) {
+        const randomNumber = Math.floor(Math.random() * (100 - 50) + 50);
+        val.ee += randomNumber;
+        val.co += randomNumber * val.greenCoef;
+        this.dis[number].dis = true;
+
         this.contract.methods.balanceOff('0x8C7Fd7c3c0f6405FB474Af45588D5b99a7206Af2').call().then(async (data) => {
           console.log('get', data);
           console.log('data[0]', data[0]);
           console.log('data[1]', data[1]);
-          val.ee = Math.floor(Math.random() * 5) + 1 + parseFloat(data[0]);
-          val.co = Math.floor(Math.random() * 5) + 1 + parseFloat(data[1]);
-          this.dis[number].dis = true;
+
           /*this.contract.methods.edit(val.ee, val.co).send({from: '0x8C7Fd7c3c0f6405FB474Af45588D5b99a7206Af2'}).then((dataEdit) => {
             console.log('change', dataEdit);
           });*/
